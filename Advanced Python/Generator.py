547ef3011334dac98cd8b966bedd8f9a2922e41e
Kavya <kavyagidaveer@gmail.com>
2025-02-14 23:13:31 +0530

    new files

--- Advanced Python/Generator.py
+++ Advanced Python/Generator.py
@@ -1,8 +1,36 @@
+def disp():
+    return 10
+    return 20
+    return 30
+
+print(disp()) #10
+print(disp()) #10
+print(disp()) #10
+
+
+def generator_function():
+    print('Hello')
+    yield 10
+    yield 20
+    yield 30
+
+ref = generator_function() #<generator object generator_function at 0x10f9c1b40>
+print(next(ref)) #10 
+print(next(ref)) #20
+print(next(ref)) #30
+#print(next(ref)) #StopIteration
+
+def fibonacci(num):
+    a,b = 0,1
+    for i in range(num):
+        print(a)
+        a,b = b, a+b
+fibonacci(10)
+
 def fibonacci_gen(num):
-    a, b = 0,1
+    a,b = 0,1
     for i in range(num):
         yield a
-        print(a)
         a,b = b, a+b
 
 ref = fibonacci_gen(1000)
--- Advanced Python/Iterator.py
+++ Advanced Python/Iterator.py
@@ -1,7 +1,10 @@
-l = [10,20,30,40]
-print(type(l), l)
-iterator_object = iter(l)
-print(type(iterator_object), iterator_object)
+li = [10,20,30,40]
+print(type(li),li)
+iterator_object = iter(li)
+print(type(iterator_object ),iterator_object)
 
 print(next(iterator_object))
 print(next(iterator_object))
+print(next(iterator_object))
+print(next(iterator_object))
+#print(next(iterator_object)) #StopIteration
