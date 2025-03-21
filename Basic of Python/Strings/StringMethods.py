387977cd8fc013da8b3d5fdcff3cd63b23334961
Kavya <kavyagidaveer@gmail.com>
2025-03-21 23:36:46 +0530

    updated files

--- Basic of Python/Strings/StringMethods.py
+++ Basic of Python/Strings/StringMethods.py
@@ -1,29 +1,5 @@
-#Different ways of Declaring the strings
-s1 = "I'm Kavya"
-s2 = 'My "Age" is 23'
-s3 = """We are Learning
-        Strings"""
-s4 = '''Python is Dynamically
-        Typed Language'''
-print(s1)
-print(s2)
-print(s3)
-print(s4)
+sentence = "This is India"
+words = sentence.split()
+words.sort()
+print(words[-2])
 
-
-# def find_pairs(nums, target):
-#     nums.sort()
-#     a = set()
-#     res = set()
-
-#     for num in nums:
-#         b = target - num
-#         if b in a:
-#             res.add(tuple(sorted((num, b))))
-#         a.add(num)
-
-#     return [list(pair) for pair in res]
-
-# nums = [1,1,2,2,3,3]
-# target = 5
-# print(find_pairs(nums, target))
